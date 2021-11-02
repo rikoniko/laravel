556f02c4fdc8ce18590ffddd1a2bf1b07ff78506
rikoniko <rikoniko_777@icloud.com>
2021-11-03 03:22:32 +0900

    Postコントローラの作成

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        
+        <title>Blog Name</title>
+
+        <!-- Fonts -->
+        
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{$post->title}}</h2>
+                    <p class='body'>{{$post->body}}</p>
+                </div>
+            @endforeach
+        </div>
+    </body>
+</html>
