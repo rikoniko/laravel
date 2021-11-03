f5fa7efe6d31519745604de78c06b861daa7e84f
rikoniko <rikoniko_777@icloud.com>
2021-11-03 11:09:45 +0900

    ページネーション追加

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+        // updated_atで降順に並べたあと、paginateで「ページごと」に表示したいアイテム数に制限する
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@
 |
 */
 
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
